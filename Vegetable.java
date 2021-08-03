feb7477323f3a2d0eacb3afeb438918d304f50c0
Sam Lijo <samuelvillalvalijo@gmail.com>
2021-08-03 17:13:15 -0500

    Updated Weapon.java

--- Vegetable.java
+++ Vegetable.java
@@ -5,5 +5,7 @@ public class Vegetable {
     public static void main(String []args){
         Vegetable garlic = new Vegetable ("Garlic");
     }
+
+
 }
 
--- Weapons.java
+++
@@ -1,4 +0,0 @@
-public class Weapons {
-    int payload;
-
-}
